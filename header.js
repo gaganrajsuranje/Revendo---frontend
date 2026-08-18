a5d97f7ec368bfcf21cf63fe8bfc85dde3b381ca
GAGANRAJ ROHIDAS SURNJE <gagan@GAGANs-MacBook-Air.local>
2026-08-18 18:03:10 +0530

    first commit

---
+++ header.js
@@ -0,0 +1,57 @@
+const headerHTML = `
+<header>
+    <div class="upper">
+
+        <a href="homepage.html" class="logo-section">
+            <i class="fa-solid fa-rotate fa-2xl"
+               style="color: rgba(23, 105, 224, 1.00);"></i>
+            <h1>ReVendo</h1>
+        </a>
+
+        <div class="search-box">
+            <form>
+                <input type="text"
+                       placeholder="Search for mobiles, accessories and more...">
+                <button type="submit">
+                    <i class="fa fa-search"></i>
+                </button>
+            </form>
+        </div>
+
+        <div class="right-section">
+
+            <div class="location-box">
+                <i class="fa fa-location-dot"></i>
+
+                <select>
+                    <option>Select Location</option>
+                    <option>Delhi</option>
+                    <option>Mumbai</option>
+                    <option>Chennai</option>
+                    <option>Bangalore</option>
+                    <option>Kolkata</option>
+                    <option>Ahmedabad</option>
+                </select>
+            </div>
+
+            <button class="login-btn">
+                <i class="fa fa-user"></i>
+                Login
+            </button>
+
+        </div>
+    </div>
+
+    <div class="main">
+        <ul>
+            <li><a href="homepage.html">Home</a></li>
+            <li><a href="buy.html">Buy</a></li>
+            <li><a href="sell.html">Sell</a></li>
+            <li><a href="aboutus.html">About Us</a></li>
+            <li><a href="contact.html">Contact Us</a></li>
+        </ul>
+    </div>
+</header>
+`;
+
+document.body.insertAdjacentHTML("afterbegin", headerHTML);
